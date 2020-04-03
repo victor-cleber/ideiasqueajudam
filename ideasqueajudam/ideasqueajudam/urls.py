4b5bd459d632a2b6e662578cf6a68e22037718ac
Victor Cleber <victor.cleber@outlook.com>
2020-04-04 00:09:36 +1300

    Adding new configuration

--- ideasqueajudam/ideasqueajudam/urls.py
+++ ideasqueajudam/ideasqueajudam/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
+
+from ideasqueajudam.ideias import urls as app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ideias.urls'))
+    path('', include(app_urls))
 ]
