947ea95b28992efc5722ec7989e82630883ba1f7
André Lopes <andrevctr12@gmail.com>
2020-04-05 08:36:03 -0300

    Enhance Heroku deploy script

--- heroku_front_deploy.sh
+++ heroku_front_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Primeiro de um commit :D
-if heroku login && heroku git:remote -a ideiasqueajudamweb; then
+heroku auth:whoami && echo "######## Already logged on Heroku #############" || heroku login
+if heroku git:remote -a ideiasqueajudamweb; then
     echo "######## Deploying on Heroku #############"
     git subtree push --prefix ideasqueajudam/front heroku master
 fi
-
