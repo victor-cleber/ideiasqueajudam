e5741321ede208e0aaecd07a24dfd3841bb628b8
Phillip Nunes <phillip_nunes@hotmail.com>
2020-04-06 19:03:25 -0300

    checkpoint

--- ideasqueajudam/front/src/services/ideas/get.js
+++ ideasqueajudam/front/src/services/ideas/get.js
@@ -2,7 +2,7 @@ import axios from '../axios'
 
 export async function getIdeas() {
   try {
-    const response = await axios.get("/ideias")
+    const response = await axios.get('/ideias')
     return response
   } catch (error) {
     console.error(error)
