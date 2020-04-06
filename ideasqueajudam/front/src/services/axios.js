74badeb9526fbb3bed2b94ef1ba908aca0fe86cf
Phillip Nunes <phillip_nunes@hotmail.com>
2020-04-06 17:59:39 -0300

    checkpoint

--- ideasqueajudam/front/src/services/axios.js
+++ ideasqueajudam/front/src/services/axios.js
@@ -4,8 +4,7 @@ const axios = require('axios')
 const instance = axios.create({
   baseURL: config.apiDomain,
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Content-Type': 'application/json'
   },
 })
 
