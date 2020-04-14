4149bc407f5b79c3d0841050c620429a28f661df
André Lopes <andrevctr12@gmail.com>
2020-04-13 21:02:12 -0300

    Remove headers

--- ideasqueajudam/front/src/services/axios.js
+++ ideasqueajudam/front/src/services/axios.js
@@ -2,11 +2,7 @@ import config from '../utils/constants/config'
 const axios = require('axios')
 
 const instance = axios.create({
-  // baseURL: config.apiDomain,
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-  },
+  baseURL: config.apiDomain,
 })
 
 export default instance
